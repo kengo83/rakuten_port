db221900cb2825a96850cf577ad440cbe027fd14
kengo83 <86722290+kengo83@users.noreply.github.com>
2021-10-02 11:21:09 +0900

    Delete urls.py

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from .views import Create,resultfunc
-
-urlpatterns = [
-    path('', Create.as_view(),name='home'),
-    path('result/',resultfunc,name='result'),
-]
